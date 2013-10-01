7e885d7de03030d52cec8c2bf4e846177816869c
MrKireko <mrkireko@gmail.com>
2013-10-01 16:48:49 +0200

    Added Site
    Added Site

---
+++ javascript.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    
+    var link1 = '#link1';
+    var link2 = '#link2';
+    
+    /*link1*/
+    $(link1).mouseenter(function(){
+        $(link1).width(175);
+        $(link2).width(65);
+        $(link2).html("<br>Some");
+    });
+    
+    $(link1).mouseleave(function(){
+        $(link1).width(120);
+        $(link2).width(120);
+        $(link2).html("<br>Something");
+    });
+    
+    $(link1).click(function(){
+        window.open("http://www.github.com/marksomnian/echode");
+    });
+    
+    /*link2*/
+    $(link2).mouseenter(function(){
+        $(link2).width(170);
+        $(link1).width(65);
+        $(link1).html("<br>GitHub");
+    });
+    
+    $(link2).mouseleave(function(){
+        $(link2).width(120);
+        $(link1).width(120);
+        $(link1).html("<br>Our GitHub");
+    });
+    $(link2).click(function(){
+        window.open("http://marksomnian.com/echode/Index.html");
+    });
+});
