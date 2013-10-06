98351f2db5068ac43076804db67294ecbe47e531
MrKireko <mrkireko@gmail.com>
2013-10-06 15:58:34 +0200

    maybe this works
    didn't work last time

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 //checking if user uses iPhone/iTouch
 if ((navigator.userAgent.indexOf('iPhone') != -1) || (navigator.userAgent.indexOf('iPod') != -1)){
-    document.location = "/mobile/";
+    document.location = "/EchodeSite/mobile/";
 }
 
 //checking if user uses iPad
