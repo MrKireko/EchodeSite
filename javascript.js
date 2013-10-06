78d2d7612f9fa3cf10100be96ae880ae875c02aa
MrKireko <mrkireko@gmail.com>
2013-10-06 17:47:06 +0200

    Now first checking for tablet, then mobile

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
-if (navigator.userAgent.indexOf('Mobile') != -1) { /*checking if user uses a non-desktop browser*/
+if ((navigator.userAgent.indexOf('iPad') != -1) || (navigator.userAgent.indexOf('Android') != -1 && navigator.userAgent.indexOf('mobile') == -1)) { /*checking if user uses a non-desktop browser*/
     document.location = "/EchodeSite/mobile/";
-}else if ((navigator.userAgent.indexOf('iPad') != -1) || (navigator.userAgent.indexOf('Android') != -1)) {
+}else if ((navigator.userAgent.indexOf('mobile') != -1)) {
     document.location = "http://www.yahoo.com/";
 }
 
