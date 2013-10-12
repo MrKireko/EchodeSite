5c49127f78478c011cb259a417d4a41062933638
MrKireko <mrkireko@gmail.com>
2013-10-12 11:51:17 +0200

    Changed about layout

--- team.js
+++ team.js
@@ -4,31 +4,58 @@ $(document).ready(function(){
     var erik = $("#erik");
     var bas = $("#bas");
     var marks = $("#marks");
+    var ae = $("#ae");
+    var ab = $("#ab");
+    var am = $("#am");
     
     
     header.click(function(){
         document.location = "Index.html";
     });
     
-    erik.mouseenter(function(){
-        $(this).css("background-color", "#ddd");
-    });
-    erik.mouseleave(function(){
-        $(this).css("background-color", "#ccc");
-    });
-    
-    bas.mouseenter(function(){
-        $(this).css("background-color", "#ddd");
-    });
-    bas.mouseleave(function(){
-        $(this).css("background-color", "#ccc");
+    ae.click(function(){
+        if (erik.css("display") == "none") {
+            erik.css("display", "inline-block");
+        }else{
+            erik.css("display", "none");
+        }
     });
     
-    marks.mouseenter(function(){
-        $(this).css("background-color", "#ddd");
+    ab.click(function(){
+        if (bas.css("display") == "none") {
+            bas.css("display", "inline-block");
+        }else{
+            bas.css("display", "none");
+        }
     });
-    marks.mouseleave(function(){
-        $(this).css("background-color", "#ccc");
+    
+    am.click(function(){
+        if (marks.css("display") == "none") {
+            marks.css("display", "inline-block");
+        }else{
+            marks.css("display", "none");
+        }
     });
+    
+    //erik.mouseenter(function(){
+    //    $(this).css("background-color", "#ddd");
+    //});
+    //erik.mouseleave(function(){
+    //    $(this).css("background-color", "#ccc");
+    //});
+    //
+    //bas.mouseenter(function(){
+    //    $(this).css("background-color", "#ddd");
+    //});
+    //bas.mouseleave(function(){
+    //    $(this).css("background-color", "#ccc");
+    //});
+    //
+    //marks.mouseenter(function(){
+    //    $(this).css("background-color", "#ddd");
+    //});
+    //marks.mouseleave(function(){
+    //    $(this).css("background-color", "#ccc");
+    //});
     
 });
