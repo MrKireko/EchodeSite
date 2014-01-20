7d11d3cde94ebcf5ec0a00b0eca236b438a5091f
MrKireko <mrkireko@gmail.com>
2014-01-20 17:31:29 +0100

    Mobile fixes
    ~ Shorter loading time for mobiles
    ~ Tablets no longer go to Yahoo!, but to desktop site

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-document.location = "desktop/index.html";
+if ((navigator.userAgent.indexOf('iPad') != -1) || (navigator.userAgent.indexOf('Android') != -1 && navigator.userAgent.indexOf('Mobile') == -1)) { /*checking if user uses a non-desktop browser*/
+    document.location = "desktop/index.html";
+}else if ((navigator.userAgent.indexOf('Mobile') != -1)) {
+    document.location = "http://mrkireko.github.io/EchodeSite/mobile/";
+}else{
+    document.location = "desktop/index.html";
+}
